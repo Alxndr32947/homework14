c612894358bf8e0b9d46e5ff80530a55d3de06c8
Alxndr32947 <alexsh32947@gmail.com>
2025-01-07 20:45:51 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,24 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        """
+        Инициализация объекта класса House.
+
+        :param name: Название дома (строка)
+        :param number_of_floors: Количество этажей (целое число)
+        """
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            print(*range(1, new_floor + 1), sep="\n")
+        else:
+            print("Такого этажа не существует")
+
+
+# Пример использования
+house1 = House("ЖК Эльбрус", 30)
+house2 = House("Коттедж", 2)
+
+house1.go_to(5)  # Вывод этажей от 1 до 5
+house2.go_to(10)  # Сообщение о недопустимом этаже
